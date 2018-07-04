d7296a77ed06e3608dc2971881cda4e55480b1bd
Abel <abel.souza.magalhaes@hotmail.com>
2018-07-03 22:12:47 -0300

    MyReader

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+import 'rxjs/add/operator/map';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public feeds: Array<string>;
+  private url : string = 'https://www.reddit.com/new.json';
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public http: Http, public loadingCtrl : LoadingController, public iab : InAppBrowser) {
+     this.fetchContent();
+  }
 
+  fetchContent():void {
+    let loading = this.loadingCtrl.create({
+      content: 'Fetching content...'
+    });
+
+    loading.present();
+
+    this.http.get(this.url).map(res => res.json()) 
+    .subscribe(data => {
+      this.feeds = data.data.children;
+      loading.dismiss();
+    })
+  }
+
+  itemSelected(url:string):void{
+    let browser = this.iab.create(url);
+    browser.show();
   }
 
 }
